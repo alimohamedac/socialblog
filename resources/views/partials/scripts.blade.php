3b5cf38c3d7138c4b4575dfd94bffd66df21598e
alimohamedac <a.mohamed666999@gmail.com>
2023-12-16 12:15:52 +0200

    Modifiy Dashboard

---
+++ resources/views/partials/scripts.blade.php
@@ -0,0 +1,3 @@
+<script src="{{ asset('assets/js/main.min.js') }}"></script>
+<script src="{{ asset('assets/js/script.js') }}"></script>
+<script src="{{ asset('assets/js/map-init.js') }}"></script>
