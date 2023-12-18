0a9b6ad6e596e7d3f326f072c3004b0528df0355
alimohamedac <a.mohamed666999@gmail.com>
2023-12-18 18:09:09 +0200

    slug col

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,6 +39,10 @@
                     <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
             </div>
+                <!-- Remember Me -->
+            <div class="block mt-4">
+                <a href="{{ route('register') }}" class="">{{ __('Join Us') }}</a>
+            </div>
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
